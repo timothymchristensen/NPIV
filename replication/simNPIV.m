d23c003e4f4b4a4c432019d3cab347fb8b38ef23
Tim Christensen <tmc8@nyu.edu>
2017-03-25 13:11:04 -0400

    Update simNPIV.m

--- replication/simNPIV.m
+++ replication/simNPIV.m
@@ -1,16 +1,5 @@
 function [Y,X,W,H0] = simNPIV(N,s2,design)
 
-% Sig  = [1 .8 .5; .8 1 0; .5 0 1];
-% R    = chol(Sig);
-% Z    = randn(N,3)*R;
-% 
-% X    = normcdf(Z(:,1));
-% W    = normcdf(Z(:,2));
-% U    = sqrt(s2)*Z(:,3);
-% H0   = H0fun(X,design);
-% Y    = H0 + U;
-
-
 Sig  = [1 .5 0; .5 1 0; 0 0 1];
 R    = chol(Sig);
 Z    = randn(N,3)*R;
@@ -21,11 +10,6 @@ W    = Z(:,3);
 X    = normcdf((W + V)/sqrt(2));
 W    = normcdf(Z(:,3));
 
-% 
-% 
-% X    = normcdf(Z(:,1));
-% W    = normcdf(Z(:,2));
-% U    = sqrt(s2)*Z(:,3);
 H0   = H0fun(X,design);
 Y    = H0 + sqrt(s2)*U;
 
